ec2ec60e2e5d4614d966039214e2eb9a22749b86
Kevin Korte <kevinkorte15@gmail.com>
2016-08-30 04:45:43 +0000

    added sass pipeline to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function() {
-    
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/*.scss', ['sass']);
 });
