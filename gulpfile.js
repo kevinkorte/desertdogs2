983d31c3b1c2bc4f11545ee324f92f2d70cbf033
Kevin Korte <kevinkorte15@gmail.com>
2016-08-31 03:45:14 +0000

    broke out over header and footer files

--- gulpfile.js
+++ gulpfile.js
@@ -14,5 +14,5 @@ gulp.task('sass', function () {
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/*.scss', ['sass']);
+  gulp.watch('./sass/partials/*.scss', ['sass']);
 });
---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Great Site</title>
+    <?php wp_head(); ?>
+  </head>
+  <body>
+  <nav class="navbar navbar-default">
+  </nav>
