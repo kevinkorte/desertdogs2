a586390ca8eb1d51119674a4b754e4301ae04477
Kevin Korte <kevinkorte15@gmail.com>
2016-09-01 05:31:10 +0000

    set up front-page, todo: break out into templates

--- front-page.php
+++ front-page.php
@@ -6,7 +6,26 @@
       Professional Dog Training
       <a href="#" class="btn btn-default">Let's Go</a>
     </div>
-    <div class="col-md-6"></div>
+    <div class="col-md-6">
+      <?php $args = array(
+        'post_type' => 'post',
+        'post_per_page' => 1
+      ); ?>
+      <?php $the_query = new WP_Query( $args ); ?>
+      <?php if ( $the_query->have_posts() ) : ?>
+      <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+		<h2><?php the_title(); ?></h2>
+	<?php endwhile; ?>
+	<!-- end of the loop -->
+
+	<!-- pagination here -->
+
+	<?php wp_reset_postdata(); ?>
+
+<?php else : ?>
+	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+    </div>
   </div>
 </div>
 
