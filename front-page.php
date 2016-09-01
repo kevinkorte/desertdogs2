acaf45057bbb92ccca5135c02a80342117fcf69b
Kevin Korte <kevinkorte15@gmail.com>
2016-09-01 05:22:21 +0000

    added material js

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-6">
+      Professional Dog Training
+      <a href="#" class="btn btn-default">Let's Go</a>
+    </div>
+    <div class="col-md-6"></div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,10 +1,11 @@
 <?php
 
 function jquery_enqueue() {
-    wp_deregister_script('jquery');
-    wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js', false, null);
-    wp_enqueue_script('jquery');
+  wp_deregister_script('jquery');
+  wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js', false, null);
+  wp_enqueue_script('jquery');
 }
+
 if (!is_admin()) add_action('wp_enqueue_scripts', 'jquery_enqueue', 11);
 
 function wp_dd_styles() {
@@ -13,6 +14,8 @@ function wp_dd_styles() {
 }
 function wp_dd_scripts() {
   wp_enqueue_script('bootstrap-js-cdn', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', false, null, true);
+  wp_enqueue_script('material-min-js', get_template_directory_uri().'/material.min.js', false, null, true);
+  wp_enqueue_script('material-js', get_template_directory_uri().'/material-kit.js', false, null, true);
 }
 
 add_action('wp_enqueue_scripts', 'wp_dd_styles');
